6e6c487c5aa658f4c5c5027f2b4c65c9ba612910
Nikolay <nikchern@gmail.com>
2021-10-04 19:16:55 -0700

    Fixed teams creation/deletion.

--- spectro-teams.tf
+++ spectro-teams.tf
@@ -1,4 +1,3 @@
-/*
 data "spectrocloud_role" "projectadmin" {
   name = "Project Admin"
 }
@@ -7,8 +6,8 @@ data "spectrocloud_role" "projectviewer" {
   name = "Project Viewer"
 }
 
-resource "spectrocloud_team" "projectadmin" {
-  for_each = module.replacer_projects.all_objects
+resource "spectrocloud_team" "admin_team" {
+  for_each = var.projects
 
   name = format("%s_admin", each.value.name)
   project_role_mapping {
@@ -17,12 +16,12 @@ resource "spectrocloud_team" "projectadmin" {
   }
 }
 
-resource "spectrocloud_team" "projectview" {
-  for_each = module.replacer_projects.all_objects
+resource "spectrocloud_team" "view_team" {
+  for_each = var.projects
 
   name = format("%s_view", each.value.name)
   project_role_mapping {
     id    = local.project_ids[each.value.name]
     roles = [data.spectrocloud_role.projectviewer.id]
   }
-}*/
+}
