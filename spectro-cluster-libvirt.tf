231bf05b53969efb68efc04eed42d7a34525ee5b
Nikolay <nikchern@gmail.com>
2022-04-04 21:29:05 -0700

    Fixing support for attached_disks_size_gb.

--- spectro-cluster-libvirt.tf
+++ spectro-cluster-libvirt.tf
@@ -113,7 +113,13 @@ resource "spectrocloud_cluster_libvirt" "this" {
         memory_mb              = machine_pool.value.memory_mb
         cpu                    = machine_pool.value.cpu
         cpus_sets              = try(machine_pool.value.cpus_sets, "")
-        attached_disks_size_gb = try(machine_pool.value.attached_disks_size_gb, "")
+        dynamic "attached_disks_size_gb" {
+          for_each = machine_pool.value.attached_disks_size_gb
+          content {
+            managed = try(attached_disks_size_gb.value.managed, false)
+            size_in_gb = attached_disks_size_gb.value.size_in_gb
+          }
+        }
       }
     }
   }
