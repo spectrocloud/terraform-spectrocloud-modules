b0f9b3293a654748441d8061c35835177dd32e64
Nikolay <nikchern@gmail.com>
2022-04-21 09:06:15 -0700

    BET-4928: adding optional namespace parameter for rbac binding

--- spectro-cluster-edge.tf
+++ spectro-cluster-edge.tf
@@ -11,6 +11,7 @@ resource "spectrocloud_cluster_edge" "this" {
     for_each = try(each.value.cluster_rbac_binding, [])
     content {
       type = cluster_rbac_binding.value.type
+      namespace  = try(cluster_rbac_binding.value.namespace, "")
 
       role = {
         kind = cluster_rbac_binding.value.role.kind
