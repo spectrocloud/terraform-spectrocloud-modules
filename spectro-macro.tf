149800436d1f34fa0524ba832d9524790251c1d6
Nikolay <nikchern@gmail.com>
2022-05-27 15:28:27 -0700

    BET-5148: Support macros resource.

---
+++ spectro-macro.tf
@@ -0,0 +1,24 @@
+locals {
+  macros = flatten([
+  for v in var.macros : [
+  for m in v.macros : {
+    name  = format("%s%%%s", m.name, v.project)
+    value = {
+      name    = m.name
+      value   = m.value
+      project = try(v.project, "")
+    }
+  }
+  ]
+  ])
+
+  macros_iterable = { for macro in local.macros: macro.name => macro.value }
+}
+
+resource "spectrocloud_macro" "macro" {
+  for_each = local.macros_iterable
+
+  name    = each.value.name
+  value   = each.value.value
+  project = each.value.project
+}
